371bdf8d2a65bfe69fd42a252c877ab9dbb05690
KomorGiaoGiao <KomorGiaoGiao@users.noreply.github.com>
2026-09-25 14:02:51 +0800

    Split mini app shell from web map UI

--- 全球家族办公室地图/pages/index/index.js
+++ 全球家族办公室地图/pages/index/index.js
@@ -1,5 +1,55 @@
 Page({
   data: {
-    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html'
+    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html?embedded=1',
+    searchOpen: false,
+    searchQuery: '',
+    detailOpen: false,
+    selectedName: '',
+    selectedDescription: ''
   },
+
+  onLoad() {
+    this.mapContext = tt.createWebViewContext('map-webview', this);
+  },
+
+  toggleSearch() {
+    this.setData({ searchOpen: !this.data.searchOpen });
+  },
+
+  handleSearchInput(event) {
+    this.setData({ searchQuery: event.detail.value });
+  },
+
+  submitSearch() {
+    const query = this.data.searchQuery.trim();
+    if (!query) return;
+
+    this.mapContext.postMessage({
+      data: { type: 'search', query }
+    });
+    this.setData({ searchOpen: false });
+  },
+
+  resetView() {
+    this.mapContext.postMessage({
+      data: { type: 'resetView' }
+    });
+    this.setData({ detailOpen: false });
+  },
+
+  closeDetail() {
+    this.resetView();
+  },
+
+  handleWebMessage(event) {
+    const messages = Array.isArray(event.detail.data) ? event.detail.data : [event.detail.data];
+    const message = messages[messages.length - 1] || {};
+    if (message.type !== 'regionSelected') return;
+
+    this.setData({
+      detailOpen: true,
+      selectedName: message.name || '已选位置',
+      selectedDescription: message.description || '已定位到地图位置'
+    });
+  }
 })
