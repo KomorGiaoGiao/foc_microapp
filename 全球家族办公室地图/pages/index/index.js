59636b8b89f13fcb849c123c9f32f14425571bbc
KomorGiaoGiao <KomorGiaoGiao@users.noreply.github.com>
2026-09-25 14:59:26 +0800

    Refresh grayscale UI in web view

--- 全球家族办公室地图/pages/index/index.js
+++ 全球家族办公室地图/pages/index/index.js
@@ -1,6 +1,6 @@
 Page({
   data: {
-    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html?embedded=1&ui=bottom-v3',
+    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html?embedded=1&ui=bottom-v4',
     searchOpen: false,
     searchQuery: '',
     detailOpen: false,
