a4f39e3b1622f9899790b077f35c0b9deebc5fe9
KomorGiaoGiao <KomorGiaoGiao@users.noreply.github.com>
2026-09-25 15:10:45 +0800

    Refresh native map shell cache

--- 全球家族办公室地图/pages/index/index.js
+++ 全球家族办公室地图/pages/index/index.js
@@ -1,6 +1,6 @@
 Page({
   data: {
-    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html?embedded=1&ui=bottom-v4',
+    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html?embedded=1&ui=bottom-v5',
     searchOpen: false,
     searchQuery: '',
     detailOpen: false,
