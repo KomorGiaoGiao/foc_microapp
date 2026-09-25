0a8bd702c1863e7d530adaf018d744b10bf5dd0c
KomorGiaoGiao <KomorGiaoGiao@users.noreply.github.com>
2026-09-25 16:42:05 +0800

    Clean webview-only map structure

--- 全球家族办公室地图/pages/index/index.js
+++ 全球家族办公室地图/pages/index/index.js
@@ -1,9 +1,5 @@
 Page({
   data: {
-    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html?standalone=1'
-  },
-
-  onLoad() {
-    this.mapContext = tt.createWebViewContext('map-webview', this);
+    mapUrl: 'https://komorgiaogiao.github.io/demo/index.html'
   }
 });
